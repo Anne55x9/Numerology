ebef270dffefe090ad738da48fea4c6aca5dfc55
anne55x9 <anne55x9@edu.easj.dk>
2016-10-11 13:51:03 +0200

    Tested the mysticnumber

--- Sandbox/InsertCodeHere.cs
+++ Sandbox/InsertCodeHere.cs
@@ -8,6 +8,13 @@ namespace Sandbox
         {
             // The FIRST line of code should be BELOW this line
 
+            MysticNumbers mysticnumber1 = new MysticNumbers();
+
+            //mysticnumber1.ThreeNumbers(4, 6, 9);
+
+            Console.WriteLine($"resultatet er: {mysticnumber1.ThreeNumbers(4, 6, 2)}");
+
+
             // The LAST line of code should be ABOVE this line
         }
     }
